2d8c7e7bf437c26e9a8f65f819c85612157fffa1
Elsaadany427 <hamadabassem427@gmail.com>
2022-11-25 18:12:37 +0200

    Initialized Constructor

--- src/H01_01_PERSON.cpp
+++ src/H01_01_PERSON.cpp
@@ -2,7 +2,14 @@
 // Created by elsaadany on 25/11/22.
 //
 
+#include <bits/stdc++.h>
 #include "../include/H01_01_PERSON.h"
 namespace personStd {
-
+    // Initialized Constructor
+    Person::Person() {
+        m_id = -1;
+        m_gender = 0;
+        m_age = 0;
+        m_category = -1;
+    }
 }
