68070631ef26b343d0ff80595746dda31aa64646
Elsaadany427 <hamadabassem427@gmail.com>
2022-11-26 10:54:56 +0200

    make doctor inherit from person and finsished doctor header

--- include/H03_03_DOCTOR.h
+++ include/H03_03_DOCTOR.h
@@ -4,10 +4,25 @@
 
 #ifndef HOSPITAL_MANAGEMENT_SYSTEM_H03_03_DOCTOR_H
 #define HOSPITAL_MANAGEMENT_SYSTEM_H03_03_DOCTOR_H
+#include "H01_01_PERSON.h"
 #include <bits/stdc++.h>
 
 namespace doctorStd {
-    class Doctor {
+    class Doctor : public personStd::Person {
+    private:
+        std::string type;
+        int appointmentBooked;
+
+    public:
+        Doctor();
+        void fillMap();
+        void saveMap();
+        void addPerson();
+        void printDetails();
+        void printDetailsFromHistory();
+        void getDetails(int t_rec = 0) = 0;
+        void getDetailsFromHistory();
+        void removePerson();
     };
-}
+}// namespace doctorStd
 #endif//HOSPITAL_MANAGEMENT_SYSTEM_H03_03_DOCTOR_H
