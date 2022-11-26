9fc8517b5896c21c80c23201524f8853b1419644
Elsaadany427 <hamadabassem427@gmail.com>
2022-11-26 10:49:29 +0200

    Added header and src of doctor and described the Goal of doctor class

---
+++ include/H03_03_DOCTOR.h
@@ -0,0 +1,13 @@
+//
+// Created by elsaadany on 26/11/22.
+//
+
+#ifndef HOSPITAL_MANAGEMENT_SYSTEM_H03_03_DOCTOR_H
+#define HOSPITAL_MANAGEMENT_SYSTEM_H03_03_DOCTOR_H
+#include <bits/stdc++.h>
+
+namespace doctorStd {
+    class Doctor {
+    };
+}
+#endif//HOSPITAL_MANAGEMENT_SYSTEM_H03_03_DOCTOR_H
---
+++ src/H03_03_DOCTOR.cpp
@@ -0,0 +1,8 @@
+//
+// Created by elsaadany on 26/11/22.
+//
+
+#include "H03_03_DOCTOR.h"
+namespace doctorStd {
+
+}
