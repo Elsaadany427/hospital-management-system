e26140fbb816cb4af126d8f47ad1dbd49192ca4e
Elsaadany427 <hamadabassem427@gmail.com>
2022-11-27 21:42:22 +0200

    Added nursesList and limits in Hospital class

--- src/H04_04_HOSPITAL.cpp
+++ src/H04_04_HOSPITAL.cpp
@@ -5,13 +5,24 @@
 #include "H04_04_HOSPITAL.h"
 #include "H03_03_DOCTOR.h"
 namespace hospitalStd {
+    //map<id, object>
     std::map<int, doctorStd::Doctor> Hospital::m_doctorsList;
+    std::map<int, nurseStd::Nurse> Hospital::m_nursesList;
+
+    // Limits
     const int Hospital::m_doctorsLimit = 30;
+    const int Hospital::m_nursesLimit = 50;
 
     //defining declared methods;
     void Hospital::printDoctors()
     {
         for (auto i : m_doctorsList)
+            i.second.printDetails(), std::cout << "\n";
+    }
+
+    void Hospital::printNurses()
+    {
+        for (auto i : m_nursesList)
             i.second.printDetails(), std::cout << "\n";
     }
 }
