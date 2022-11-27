0430084daf0d3dec13ae0260d0631b2e9f6fd2da
Elsaadany427 <hamadabassem427@gmail.com>
2022-11-27 21:48:20 +0200

    Implemented saveMap method

--- src/H05_05_NURSE.cpp
+++ src/H05_05_NURSE.cpp
@@ -26,9 +26,9 @@ namespace nurseStd {
             Nurse n;
             //creating a string stream object to read from string 'temp';
             std::stringstream s(copyDataFromFileNurse);
-            std::string doctorId, gender, age, address;
+            std::string nurseId, gender, age, address;
             //reading from the string stream object 's';
-            getline(s, doctorId, ',');
+            getline(s, nurseId, ',');
             getline(s, n.m_firstName, ',');
             getline(s, n.m_lastName, ',');
             getline(s, gender, ',');
@@ -40,7 +40,23 @@ namespace nurseStd {
             n.m_gender = gender[0];
             n.m_age = {(int16_t) (globalStd::strToNum(age))};
             n.m_address.decryptAddress(address);
+            hospitalStd::Hospital::m_nursesList[n.m_id] = n;
         }
         f.close();
+    }
+    void Nurse::saveMap() {
+        std::fstream f;
+        f.open("/media/elsaadany/Data/OOP/Example/hospital-management-system/data/temp.csv", std::ios::out);
+        // `le first line containing column headers:
+        f << "nurseId,firstName,lastName,gender,age,mobNumber,address,type\n";
+
+        for (const auto &i: hospitalStd::Hospital::m_nursesList)
+            f << i.second.m_id << "," << i.second.m_firstName << "," << i.second.m_lastName << "," << i.second.m_gender
+              << "," << i.second.m_age << "," << i.second.m_mobNumber << "," << i.second.m_address.encryptAddress()
+              << "," << i.second.m_type << "\n";
+        f.close();
+
+        remove("/media/elsaadany/Data/OOP/Example/hospital-management-system/data/nurses.csv");
+        rename("/media/elsaadany/Data/OOP/Example/hospital-management-system/data/temp.csv", "/media/elsaadany/Data/OOP/Example/hospital-management-system/data/nurses.csv");
     }
 }// namespace nurseStd
