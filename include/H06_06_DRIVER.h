c9b415694b12f42d1c5a58d4f293a8a711f7d33c
Elsaadany427 <hamadabassem427@gmail.com>
2022-11-28 12:45:43 +0200

    Created Driver header and src

---
+++ include/H06_06_DRIVER.h
@@ -0,0 +1,14 @@
+//
+// Created by elsaadany on 28/11/22.
+//
+
+#ifndef HOSPITAL_MANAGEMENT_SYSTEM_H06_06_DRIVER_H
+#define HOSPITAL_MANAGEMENT_SYSTEM_H06_06_DRIVER_H
+#include "H01_01_PERSON.h"
+#include <bits/stdc++.h>
+
+namespace driverStd {
+    class Driver : public personStd::Person {
+    };
+}
+#endif//HOSPITAL_MANAGEMENT_SYSTEM_H06_06_DRIVER_H
---
+++ src/H06_06_DRIVER.cpp
@@ -0,0 +1,8 @@
+//
+// Created by elsaadany on 28/11/22.
+//
+
+#include "H06_06_DRIVER.h"
+namespace driverStd {
+
+}
